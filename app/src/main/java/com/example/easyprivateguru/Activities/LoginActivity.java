07a63a67bc3df4ed5bb923077448829b6b20b578
Rafi <pilotrogue@gmail.com>
2020-02-24 08:01:05 +0700

    google sign in sudah bisa

--- app/src/main/java/com/example/easyprivateguru/Activities/LoginActivity.java
+++ app/src/main/java/com/example/easyprivateguru/Activities/LoginActivity.java
@@ -1,19 +1,34 @@
 package com.example.easyprivateguru.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.easyprivateguru.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnSignIn;
-//    private SignInButton btnSignIn;
+//    private Button btnSignIn;
+    private SignInButton btnSignIn;
+    private GoogleSignInAccount account;
+    private static final String TAG = "LoginActivity";
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient client;
+
+    private int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +45,52 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void homeIntent(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        client = GoogleSignIn.getClient(this, gso);
+
+        if(account != null){
+            homeIntent();
+        }
+    }
+
     private void init(){
         btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignIn.setSize(SignInButton.SIZE_WIDE);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
     }
 
     private void signIn(){
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        Intent signInIntent = client.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task){
+        try {
+            account = task.getResult(ApiException.class);
+            homeIntent();
+        }catch (ApiException e){
+            Log.w(TAG, "signInResult: "+e.getStatusCode());
+        }
     }
 }
--- app/src/main/java/com/example/easyprivateguru/Fragments/ProfilFragment.java
+++ app/src/main/java/com/example/easyprivateguru/Fragments/ProfilFragment.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.easyprivateguru.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class ProfilFragment extends Fragment {
+    TextView tvProfile;
+    GoogleSignInAccount account;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,11 +26,18 @@ public class ProfilFragment extends Fragment {
         Context mContext = v.getContext();
 
         init(v);
+        account = GoogleSignIn.getLastSignedInAccount(mContext);
+
+        if(account != null){
+            tvProfile.setText("Email \t: "+account.getEmail()+"\n"+"Name \t: "+account.getDisplayName());
+        }else {
+            tvProfile.setText("No one signed in");
+        }
 
         return v;
     }
 
     private void init(View v){
-
+        tvProfile = v.findViewById(R.id.tvProfile);
     }
 }
